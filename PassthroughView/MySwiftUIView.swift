6f423419ef0e5e468def5ea3f71b9552798a3505
Muhammad Wasiq <wasiq.nisar@jeeny.me>
2025-10-26 16:57:18 +0500

    passthrughView

---
+++ PassthroughView/MySwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  MySwiftUIView.swift
+//  PassthroughView
+//
+//  Created by WasiqNisar on 23/10/2025.
+//
+
+import SwiftUI
+
+struct MySwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MySwiftUIView()
+}
